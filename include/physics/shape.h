d6ef5fbf18d42ab9040079bead8e8e1496957d69
philippark <philippark271@gmail.com>
2024-03-15 22:18:59 -0400

    condensed shape class to body class

--- include/physics/shape.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHAPE_H
-#define SHAPE_H
-#include <SFML/Graphics.hpp>
-
-
-
-#endif
--- tests/shapetest.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-#include "../include/physics/body.h"
-
-int main(){
-    std::cout << "ShapeTest" << std::endl;
-}
