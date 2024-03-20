d49e662dc802a809c05967330b4fc287e91d5636
philippark <philippark271@gmail.com>
2024-03-20 17:52:13 -0400

    2 seperate bodies

---
+++ tests/collision_test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "../include/physics/world.h"
+#include "../include/physics/body.h"
+#include "../include/physics/collision.h"
+#include <SFML/Graphics.hpp>
+
+int main(){
+
+    sf::CircleShape circle(50);
+
+    physics::Body body(circle, 0);
+
+    circle.move(100, 100);
+    physics::Body body2(circle, 0);
+
+    
+    physics::World world;
+    world.addBody(body);
+    world.addBody(body2);
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    float timestep = 0.1;
+
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+
+        world.update_world(timestep);
+        std::vector<physics::Body> bodies = world.get_bodies();
+
+        for (int i = 0; i < bodies.size(); i++){
+            window.draw(bodies[i].getShape());
+        }
+        
+
+        window.display();
+    }
+
+
+    return 0;
+}
