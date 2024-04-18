32a2999b2cd1628ead26f90e43fd8e4ebb60c3bf
philippark <philippark271@gmail.com>
2024-04-17 21:33:14 -0400

    deleted old body code

--- include/physics/body.h
+++ include/physics/body.h
@@ -2,36 +2,7 @@
 #define BODY_H
 #include <SFML/Graphics.hpp>
 
-namespace physics {
-    /*
-    class Body{
-    public:
-        Body(sf::CircleShape shape_, float mass_) {shape = shape_; mass = mass_;}
-
-        void move(float timestep);
-
-        void set_position(float x, float y);
-        void set_x_velocity(int x_velocity_) {x_velocity = x_velocity_;}
-        void set_y_velocity(int y_velocity_) {y_velocity = y_velocity_;}
-
-        void set_color(sf::Color color) {shape.setFillColor(color);}
-
-        int get_x_velocity() {return x_velocity;}
-        int get_y_velocity() {return y_velocity;}
-        sf::Vector2f get_position() {return shape.getPosition();}
-
-        sf::CircleShape& getShape() {return shape;}
-    private:
-        sf::CircleShape shape;
-        float mass = 0;
-        float x_acceleration = 0;
-        float y_acceleration = 0;
-        float x_velocity = 0;
-        float y_velocity = 0;
-
-    };
-    */
-
+namespace physics { 
     class Body : public sf::CircleShape {
         public:
         Body(float radius, float mass_) {this->setRadius(radius); mass = mass_;}
@@ -42,7 +13,7 @@ namespace physics {
         float get_y_velocity() {return y_velocity;}
 
         private:
-        float mass = 0;
+        float mass = 1;
         float x_acceleration = 0;
         float y_acceleration = 0;
         float x_velocity = 0;
--- src/physics/body.cpp
+++ src/physics/body.cpp
@@ -1,8 +1,3 @@
 #include "../../include/physics/body.h"
 
 #include <iostream>
-/*
-void physics::Body::move(float timestep) {
-    this->move(x_velocity * timestep, y_velocity * timestep);
-}
-*/
